579fb114042a924d615bf9dfd17cc1f93fa8605c
nestorsando <nestorsando45@gmail.com>
2021-01-30 15:46:03 -0500

    curso udemy v2

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://rootmongodb:J27f6bwZn7DvNe6@cluster0.ws1vt.mongodb.net/test';
+    
+}
+process.env.URLDB = urlDB;
